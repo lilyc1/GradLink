c8d05a3980188fbdad300ef325678995458cc7e1
neyaarajan <neyaarajan@ufl.edu>
2024-12-01 23:55:53 -0500

    message input changes

--- src/app/chat-area/chat-content/message-input/message-input.component.ts
+++ src/app/chat-area/chat-content/message-input/message-input.component.ts
@@ -1,16 +1,37 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { AuthService } from 'src/auth.service';
+import { DatabaseService } from 'backend/database.service';
 
 @Component({
   selector: 'app-message-input',
   templateUrl: './message-input.component.html',
   styleUrls: ['./message-input.component.css']
 })
-export class MessageInputComponent {
+export class MessageInputComponent implements OnInit{
+  userProfile: any; // Object to hold profile data
+  userId: string | null = '';
+
+  constructor(
+    private authService: AuthService,
+    private dbService: DatabaseService
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe((user) => {
+      if (user) {
+        this.userId = user.uid;
+        this.dbService.getUserProfile(this.userId).subscribe((profile) => {
+          this.userProfile = profile; // Assign fetched profile data
+        });
+      }
+    });
+  }
+
   @Output() sentMessage = new EventEmitter<Message>();
 
   send(content: string) {
     const newMessage: Message = {
-      username: 'You', // Replace with actual username logic
+      username: this.userProfile.firstName, // Replace with actual username logic
       content: content,
       timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
     };
